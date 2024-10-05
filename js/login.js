332e8c70755cf1313104d07ed34429e71198c8a9
Shuvojit Tikader <shuvojittikader@gmail.com>
2024-10-05 22:56:53 +0600

    love

---
+++ js/login.js
@@ -0,0 +1,11 @@
+// button id
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+
+    
+
+    // step-3
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber)
+})
