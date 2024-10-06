8f8506f69d081b577f85fadc1ac7c8f4def23159
Shuvojit Tikader <shuvojittikader@gmail.com>
2024-10-07 02:31:25 +0600

    3rd commit

--- js/login.js
+++ js/login.js
@@ -7,5 +7,17 @@ document.getElementById('button-login').addEventListener('click', function(event
 
     // step-3
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value ;
+    console.log(phoneNumber , pinNumber)
+        // it not right 
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are logged in');
+        window.location.href = '/home.html';
+    }
+else{
+    alert('Worng phone number or pin')
+}
+
 })
+
+
